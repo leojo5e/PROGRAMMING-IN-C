5e36e317dc7672a6e66529022652d8786d0c24a9
leojo5e <96808081+leojo5e@users.noreply.github.com>
2021-12-28 23:38:05 -0800

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,24 @@
+/*
+  *     *     *     *     *
+  *     *     *     *     *
+  *     *     *     *     *
+  *     *     *     *     *
+  *     *     *     *     *
+  */
+
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("Enter the no.of Rows");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+   
+}
