c53bf63365d14243a3ec9ec25377d47e71b0309b
leojo5e <96808081+leojo5e@users.noreply.github.com>
2022-01-18 09:22:50 -0800

    Add files via upload

---
+++ multiarray3x3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a[3][3],i,j;
+    printf("Enter the elements in the array");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+}
