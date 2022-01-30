77e674bc8597d465a06b1a63a295b76bdc75c021
leojo5e <96808081+leojo5e@users.noreply.github.com>
2022-01-29 21:23:29 -0800

    Add files via upload

---
+++ mallocsum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int *ptr,i,n,sum=0;
+    printf("Enter the no.of elements");
+    scanf("%d",&n);
+    ptr=(int*)malloc(n*sizeof(int));
+    printf("Enter the elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",ptr+i);
+        sum=sum+*(ptr+i);
+    }
+    printf("Sum=%d",sum);
+}
